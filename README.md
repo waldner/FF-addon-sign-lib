45e7032629bf3b27095fa6bed70ad0fc1414e0fe
DB <davide.brini@bsc.es>
2018-04-21 12:10:33 +0200

    refactor

--- README.md
+++ README.md
@@ -16,19 +16,19 @@ No special installation needed. Just put **`ff-sign-lib.sh`** wherever you want.
 
 - Find out your Mozilla API credentials. You can find them by logging in to Mozilla, then going to your profile amd than "tools" -> "Manage API keys". You need the **JWT issuer** and **JWT secret** values.
 
-- Implement a function called `ff_get_userdef_credentials` that sets some environment variables with suitable values (`ff_jwt_issuer` and `ff_jwt_secret` from the previous step).
+- Implement a function called `ff_get_userdef_credentials` that sets some environment variables with suitable values (`ff_lib['jwt_issuer']` and `ff_lib['jwt_secret']` from the previous step).
 
 - Source `ff-sign-lib.sh` in your script
 
 - Call `ff_api_init` passing the location of your addon as argument, and check that it returns without errors.
 
-- At this point, you can perform API operations by invoking functions like `ff_upload_file`, `iff_wait_validation` and others. See below for more.
+- At this point, you can perform API operations by invoking functions like `ff_upload_file`, `ff_wait_validation` and others. See below for more.
 
 - The whole process does not modify any file in your original source directory, so if you want to modify the manifest.json to include the new addon version, call `ff_accept_new_version` to do it.
 
 ## Internals
 
-After each API function invocation, the three variables `ff_last_http_headers`, `ff_last_http_body` and `ff_last_http_code` contain what their name says, so they can be inspected in your code for extra control.
+After each API function invocation, the three variables `ff_lib['last_http_headers']`, `ff_lib['last_http_body']` and `ff_lib['last_http_code']` contain what their name says, so they can be inspected in your code for extra control.
 
 Each file API operation ends up invoking `ff_do_curl` internally, after setting the appropriate global variables (via `ff_parse_args`).
 
@@ -39,8 +39,8 @@ Each file API operation ends up invoking `ff_do_curl` internally, after setting 
 #!/bin/bash
 
 ff_get_userdef_credentials(){
-  ff_jwt_issuer='yyyy:yyyyyyyy:yyy'
-  ff_jwt_secret='xxxxxxxxxxxxxxxxxxxxxxx'
+  ff_lib['jwt_issuer']='yyyy:yyyyyyyy:yyy'
+  ff_lib['jwt_secret']='xxxxxxxxxxxxxxxxxxxxxxx'
 }
 
 . ff-sign-lib.sh
@@ -60,12 +60,12 @@ new_version=10.1
 
 ff_upload_addon -v "$new_version" || { echo "Error uploading addon, terminating" >&2 && exit 1; }
 
-result_json=$ff_last_http_body
+result_json=${ff_lib['last_http_body']}
 
-# this sets $ff_addown_download_url if successful
+# this sets ff_lib['addown_download_url'] if successful
 ff_wait_validation -j "$result_json" || { echo "Error during validation/review, terminating" >&2 && exit 1; }
 
-ff_download_xpi_file -u "$ff_addon_download_url" -v "$new_version"
+ff_download_xpi_file -u "${ff_lib['addon_download_url']}" -v "$new_version"
 
 # write new version to manifest
 ff_accept_new_version -v "$new_version"
--- ff-sign-lib.sh
+++ ff-sign-lib.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+declare -A ff_lib=()
+
 ff_get_curtime(){
   # avoid spawning a process if we have a capable bash
   if [ ${BASH_VERSINFO[0]} -ge 4 ] && [ ${BASH_VERSINFO[1]} -ge 2 ]; then
     printf '%(%Y-%m-%d %H:%M:%S)T\n' -1
   else
-    $perl -MTime::localtime -e '$tm = localtime; printf("%04d-%02d-%02d %02d:%02d:%02d\n", $tm->year+1900, ($tm->mon)+1, $tm->mday, $tm->hour, $tm->min, $tm->sec);'
+    "${ff_lib['date']}" +"%Y-%m-%d %H:%M:%S"
   fi
 }
 
@@ -21,18 +23,20 @@ ff_check_required_binaries(){
 
   local retcode=0
 
-  curl=$(command -v curl)
-  perl=$(command -v perl)
-  zip=$(command -v zip)
-  jq=$(command -v jq)
-  openssl=$(command -v openssl)
+  ff_lib['curl']=$(command -v curl)
+  ff_lib['date']=$(command -v date)
+  ff_lib['awk']=$(command -v awk)
+  ff_lib['zip']=$(command -v zip)
+  ff_lib['jq']=$(command -v jq)
+  ff_lib['openssl']=$(command -v openssl)
 
-  ( [ "$curl" != "" ] && \
-    [ "$perl" != "" ] && \
-    [ "$openssl" != "" ] && \
-    [ "$jq" != "" ] && \
-    [ "$zip" != "" ] ) || \
-  { ff_log "Cannot find needed binaries, make sure you have curl, perl and zip in your PATH" && return 1; }
+  ( [ "${ff_lib['curl']}" != "" ] && \
+    [ "${ff_lib['date']}" != "" ] && \
+    [ "${ff_lib['awk']}" != "" ] && \
+    [ "${ff_lib['openssl']}" != "" ] && \
+    [ "${ff_lib['jq']}" != "" ] && \
+    [ "${ff_lib['zip']}" != "" ] ) || \
+  { ff_log "Cannot find needed binaries, make sure you have curl, awk, zip, openssl and jq in your PATH" && return 1; }
 }
 
 ff_get_credentials(){
@@ -42,62 +46,62 @@ ff_get_credentials(){
   local ff_userdef_cred_function=ff_get_userdef_credentials
 
   if ! declare -F $ff_userdef_cred_function >/dev/null; then
-    ff_log "Function '$ff_userdef_cred_function()' does not exist, you must define it and make sure it sets variables 'ff_jwt_issuer', 'ff_jwt_secret'"
+    ff_log "Function '$ff_userdef_cred_function()' does not exist, you must define it and make sure it sets variables 'ff_lib[jwt_issuer]', 'ff_lib[jwt_secret]'"
     return 1
   fi
 
   $ff_userdef_cred_function   # user MUST implement this
 
-  ( [ "$ff_jwt_issuer" != "" ] && \
-    [ "$ff_jwt_secret" != "" ] ) || \
+  ( [ "${ff_lib['jwt_issuer']}" != "" ] && \
+    [ "${ff_lib['jwt_secret']}" != "" ] ) || \
 
-    { ff_log "Cannot get hubic credentials; make sure '$ff_userdef_cred_function()' sets variables 'ff_jwt_issuer', 'ff_jwt_secret'" && return 1; }
+    { ff_log "Cannot get hubic credentials; make sure '$ff_userdef_cred_function()' sets variables 'ff_lib[jwt_issuer]', 'ff_lib[jwt_secret]'" && return 1; }
 }
 
 ff_api_init(){
 
-  ff_api_initialized="0"
+  ff_lib['api_initialized']="0"
   ff_log "API initialization starting..."
 
   ff_check_required_binaries || return 1
   ff_get_credentials || return 1
 
-  ff_addon_dir=$1
+  ff_lib['addon_dir']=$1
 
-  if [ "$ff_addon_dir" = "" ]; then
+  if [ "${ff_lib['addon_dir']}" = "" ]; then
     ff_log "Addon dir not given, returning"
     return 1
   fi
 
-  if [ ! -d "$ff_addon_dir" ]; then
-    ff_log "Addon dir $ff_addon_dir not found, returning"
+  if [ ! -d "${ff_lib['addon_dir']}" ]; then
+    ff_log "Addon dir ${ff_lib['addon_dir']} not found, returning"
     return 1
   fi
 
-  local manifest="$ff_addon_dir/manifest.json"
+  local manifest="${ff_lib['addon_dir']}/manifest.json"
 
   if [ ! -f "$manifest" ]; then
-    ff_log "Manifest not found in $ff_addon_dir, returning"
+    ff_log "Manifest not found in ${ff_lib['addon_dir']}, returning"
     return 1
   fi
 
   # extract addon ID and version
-  ff_addon_id=$(jq -r '.applications.gecko.id' < "$manifest")
-  if [ "$ff_addon_id" = "" ]; then
+  ff_lib['addon_id']=$(${ff_lib['jq']} -r '.applications.gecko.id' < "$manifest")
+  if [ "${ff_lib['addon_id']}" = "" ]; then
     ff_log "Cannot find addon ID in $manifest, terminating"
     return 1
   fi
 
-  ff_current_addon_version=$(jq -r '.version' < "$manifest")
+  ff_lib['current_addon_version']=$(${ff_lib['jq']} -r '.version' < "$manifest")
 
-  if [ "$ff_current_addon_version" = "" ]; then
+  if [ "${ff_lib['current_addon_version']}" = "" ]; then
     ff_log "Cannot find addon version in $manifest, terminating"
     return 1
   fi
 
   ff_log "API initialization completed"
 
-  ff_api_initialized="1"
+  ff_lib['api_initialized']="1"
 
 }
 
@@ -111,14 +115,14 @@ ff_do_jwt(){
   local end_t=$(( start_t + 60 ))
 
   local jwt_header_json='{ "alg": "HS256", "typ": "JWT" }' #, "kid": "0001"}'
-  local jwt_payload_json='{ "iss": "'"${ff_jwt_issuer}"'", "jti": "'"${jti_nonce}"'", "iat": '"${start_t}"', "exp": '"${end_t}"' }'
-  local jwt_hp_base64=$(printf '%s' "${jwt_header_json}" | $openssl base64 -A).$(printf '%s' "${jwt_payload_json}" | $openssl base64 -A)
-  local signature=$(printf '%s' "${jwt_hp_base64}" | $openssl dgst -binary -sha256 -hmac "${ff_jwt_secret}" | $openssl base64 -A)
+  local jwt_payload_json='{ "iss": "'"${ff_lib['jwt_issuer']}"'", "jti": "'"${jti_nonce}"'", "iat": '"${start_t}"', "exp": '"${end_t}"' }'
+  local jwt_hp_base64=$(printf '%s' "${jwt_header_json}" | ${ff_lib['openssl']} base64 -A).$(printf '%s' "${jwt_payload_json}" | ${ff_lib['openssl']} base64 -A)
+  local signature=$(printf '%s' "${jwt_hp_base64}" | ${ff_lib['openssl']} dgst -binary -sha256 -hmac "${ff_lib['jwt_secret']}" | ${ff_lib['openssl']} base64 -A)
   echo "${jwt_hp_base64}.${signature}"
 }
 
 ff_check_api_initialized(){
-  if [ "$ff_api_initialized" != "1" ] || [ "$ff_jwt_issuer" = "" ] || [ "$ff_jwt_secret" = "" ]; then
+  if [ "${ff_lib['api_initialized']}" != "1" ] || [ "${ff_lib['jwt_issuer']}" = "" ] || [ "${ff_lib['jwt_secret']}" = "" ]; then
     ff_log "API not initialized, call ff_api_init first"
     return 1
   fi
@@ -129,49 +133,49 @@ ff_do_curl(){
   local result
 
   result=$(
-    $curl -H "Expect:" -s -D- "$@" 
+    ${ff_lib['curl']} -H "Expect:" -s -D- "$@" 
   )
 
-  ff_last_http_headers=$($perl -pe 'exit if /^\r$/;' <<< "$result")
-  ff_last_http_body=$($perl -ne 'print if $ok; $ok = 1 if /^\r$/;' <<< "$result")
-  ff_last_http_code=$($perl -ne 'if ($_ =~ /^HTTP/) { print ((split())[1]); exit };' <<< "$result")
+  ff_lib['last_http_headers']=$(${ff_lib['awk']} '/^\r$/{ exit }1' <<< "$result")
+  ff_lib['last_http_body']=$(${ff_lib['awk']} 'ok; /^\r$/ { ok = 1 }' <<< "$result")
+  ff_lib['last_http_code']=$(${ff_lib['awk']} '/^HTTP/ { print $2; exit }' <<< "$result")
 
 }
 
 ff_parse_args(){
 
-  ff_new_addon_version=
-  ff_xpi_file=
-  ff_zip_file=
-  ff_result_json=
-  ff_addon_download_url=
-  ff_addon_listed=
+  ff_lib['new_addon_version']=
+  ff_lib['xpi_file']=
+  ff_lib['zip_file']=
+  ff_lib['result_json']=
+  ff_lib['addon_download_url']=
+  ff_lib['addon_listed']=
 
   while [ $# -gt 0 ]; do
 
     case $1 in
       -v|--version)
-        ff_new_addon_version=$2
+        ff_lib['new_addon_version']=$2
         shift 2
         ;;
       -x|--xpi-file)
-        ff_xpi_file=$2
+        ff_lib['xpi_file']=$2
         shift 2
         ;;
       -z|--zip-file)
-        ff_zip_file=$2
+        ff_lib['zip_file']=$2
         shift 2
         ;;
       -j|--json)
-        ff_result_json=$2
+        ff_lib['result_json']=$2
         shift 2
         ;;
       -u|--xpi-url)
-        ff_addon_download_url=$2
+        ff_lib['addon_download_url']=$2
         shift 2
         ;;
       -l|--listed)
-        ff_addon_listed=$2
+        ff_lib['addon_listed']=$2
         shift 2
         ;;
  
@@ -183,10 +187,10 @@ ff_parse_args(){
 
   done
 
-  if [[ ! "$ff_addon_listed" =~ ^1$ ]]; then
-    ff_addon_listed="unlisted"
+  if [[ ! "${ff_lib['addon_listed']}" =~ ^1$ ]]; then
+    ff_lib['addon_listed']="unlisted"
   else
-    ff_addon_listed="listed"
+    ff_lib['addon_listed']="listed"
   fi
 }
 
@@ -199,13 +203,13 @@ ff_upload_addon(){
   ff_create_zip "$@" || return 1
 
   local jwt_token=$(ff_do_jwt)
-  ff_do_curl -g -XPUT "https://addons.mozilla.org/api/v3/addons/${ff_addon_id}/versions/${ff_new_addon_version}/" -H "Authorization: JWT ${jwt_token}" --form "upload=@${ff_zip_file}" --form "channel=${ff_addon_listed}"
+  ff_do_curl -g -XPUT "https://addons.mozilla.org/api/v3/addons/${ff_lib['addon_id']}/versions/${ff_lib['new_addon_version']}/" -H "Authorization: JWT ${jwt_token}" --form "upload=@${ff_lib['zip_file']}" --form "channel=${ff_lib['addon_listed']}"
 
-  rm -rf "$ff_zip_file"
+  rm -rf "${ff_lib['zip_file']}"
 
-  ff_log "Upload HTTP code: $ff_last_http_code"
+  ff_log "Upload HTTP code: ${ff_lib['last_http_code']}"
 
-  if [ "$ff_last_http_code" != "201" ] && [ "$ff_last_http_code" != "202" ]; then
+  if [ "${ff_lib['last_http_code']}" != "201" ] && [ "${ff_lib['last_http_code']}" != "202" ]; then
     return 1
   else
     return 0
@@ -219,35 +223,35 @@ ff_create_zip(){
 
   ff_parse_args "$@"
 
-  if [ "$ff_new_addon_version" = "" ]; then    
+  if [ "${ff_lib['new_addon_version']}" = "" ]; then    
     ff_log "New addon version not specified, terminating"
     return 1
   fi
 
-  local manifest="$ff_addon_dir/manifest.json"
+  local manifest="${ff_lib['addon_dir']}/manifest.json"
 
-  if [ "$ff_zip_file" = "" ]; then
-    ff_zip_file=/tmp/${ff_addon_id}-${ff_new_addon_version}.zip
+  if [ "${ff_lib['zip_file']}" = "" ]; then
+    ff_lib['zip_file']=/tmp/${ff_lib['addon_id']}-${ff_lib['new_addon_version']}.zip
   fi
 
-  local addon_temp_dir=/tmp/${ff_addon_id}-${ff_new_addon_version}
+  local addon_temp_dir=/tmp/${ff_lib['addon_id']}-${ff_lib['new_addon_version']}
 
-  rm -rf "$ff_zip_file" "$addon_temp_dir"
+  rm -rf "${ff_lib['zip_file']}" "$addon_temp_dir"
 
-  cp -a "$ff_addon_dir" "$addon_temp_dir"
+  cp -a "${ff_lib['addon_dir']}" "$addon_temp_dir"
   
   local temp_manifest=$addon_temp_dir/manifest.json
-  local new_manifest=$($jq --arg new_version "$ff_new_addon_version" '.version = $new_version' < "$temp_manifest")
+  local new_manifest=$(${ff_lib['jq']} --arg new_version "${ff_lib['new_addon_version']}" '.version = $new_version' < "$temp_manifest")
   echo "$new_manifest" > "$temp_manifest"
 
-  ( cd "$addon_temp_dir" && $zip -q -r -FS "$ff_zip_file" .)
+  ( cd "$addon_temp_dir" && ${ff_lib['zip']} -q -r -FS "${ff_lib['zip_file']}" .)
 
   local result=$?
 
   rm -rf "$addon_temp_dir"
 
   if [ $result -ne 0 ]; then
-    ff_log "Error creating zip file $ff_zip_file, terminating"
+    ff_log "Error creating zip file ${ff_lib['zip_file']}, terminating"
     return 1
   fi
 
@@ -259,33 +263,33 @@ ff_wait_validation(){
 
   ff_parse_args "$@"
 
-  if [ "$ff_result_json" = "" ]; then
+  if [ "${ff_lib['result_json']}" = "" ]; then
     ff_log "No JSON passed for validation, terminating"
     return 1
   fi
   
-  local new_addon_version=$($jq -r '.version' <<< "$ff_result_json")
+  local new_addon_version=$(${ff_lib['jq']} -r '.version' <<< "${ff_lib['result_json']}")
 
   if [ "$new_addon_version" = "" ]; then
     ff_log "Cannot find addon version in the JSON, terminating"
     return 1
   fi
 
-  ff_log "Waiting for validation of addon $ff_addon_id, version $new_addon_version"
+  ff_log "Waiting for validation of addon ${ff_lib['addon_id']}, version $new_addon_version"
 
-  local automated_signing=$($jq -r '.automated_signing' <<< "$ff_result_json")
+  local automated_signing=$(${ff_lib['jq']} -r '.automated_signing' <<< "${ff_lib['result_json']}")
 
   while true; do
 
-    local processed=$($jq -r '.processed' <<< "$ff_result_json")
-    local valid=$($jq -r '.valid' <<< "$ff_result_json")
-    local validation_results=$($jq '.validation_results' <<< "$ff_result_json")
-    local reviewed=$($jq -r '.reviewed' <<< "$ff_result_json")
-    local passed_review=$($jq -r '.passed_review' <<< "$ff_result_json")
-    local success=$($jq -r '.validation_results.success' <<< "$ff_result_json")
-    local errors=$($jq -r '.validation_results.errors' <<< "$ff_result_json")
-    local warnings=$($jq -r '.validation_results.warnings' <<< "$ff_result_json")
-    local download_url=$($jq -r '.files[0].download_url' <<< "$ff_result_json")
+    local processed=$(${ff_lib['jq']} -r '.processed' <<< "${ff_lib['result_json']}")
+    local valid=$(${ff_lib['jq']} -r '.valid' <<< "${ff_lib['result_json']}")
+    local validation_results=$(${ff_lib['jq']} '.validation_results' <<< "${ff_lib['result_json']}")
+    local reviewed=$(${ff_lib['jq']} -r '.reviewed' <<< "${ff_lib['result_json']}")
+    local passed_review=$(${ff_lib['jq']} -r '.passed_review' <<< "${ff_lib['result_json']}")
+    local success=$(${ff_lib['jq']} -r '.validation_results.success' <<< "${ff_lib['result_json']}")
+    local errors=$(${ff_lib['jq']} -r '.validation_results.errors' <<< "${ff_lib['result_json']}")
+    local warnings=$(${ff_lib['jq']} -r '.validation_results.warnings' <<< "${ff_lib['result_json']}")
+    local download_url=$(${ff_lib['jq']} -r '.files[0].download_url' <<< "${ff_lib['result_json']}")
 
     ff_log "Processed: $processed, valid: $valid, reviewed: $reviewed, passed_review: $passed_review"
 
@@ -310,7 +314,7 @@ ff_wait_validation(){
 
             if [ "$download_url" != "null" ]; then
               ff_log "Signing completed, download URL is $download_url"
-              ff_addon_download_url=$download_url
+              ff_lib['addon_download_url']=$download_url
               return
             else
               ff_log "Download URL not yet available..."
@@ -322,9 +326,9 @@ ff_wait_validation(){
 
     sleep 3
     local jwt_token=$(ff_do_jwt)
-    ff_do_curl -g -XGET "https://addons.mozilla.org/api/v3/addons/${ff_addon_id}/versions/${new_addon_version}/" -H "Authorization: JWT ${jwt_token}"
-    ff_log "Status after retrying is $ff_last_http_code"
-    ff_result_json=$ff_last_http_body
+    ff_do_curl -g -XGET "https://addons.mozilla.org/api/v3/addons/${ff_lib['addon_id']}/versions/${new_addon_version}/" -H "Authorization: JWT ${jwt_token}"
+    ff_log "Status after retrying is ${ff_lib['last_http_code']}"
+    ff_lib['result_json']=${ff_lib['last_http_body']}
   done
 
 }
@@ -335,29 +339,25 @@ ff_download_xpi_file(){
 
   ff_parse_args "$@"
 
-  if [ "$ff_new_addon_version" = "" ]; then
+  if [ "${ff_lib['new_addon_version']}" = "" ]; then
     ff_log "Must specify version to download"
     return 1
   fi
 
-  if [ "$ff_xpi_file" = "" ]; then
-    ff_xpi_file=/tmp/${ff_addon_id}-${ff_new_addon_version}.xpi
+  if [ "${ff_lib['xpi_file']}" = "" ]; then
+    ff_lib['xpi_file']=/tmp/${ff_lib['addon_id']}-${ff_lib['new_addon_version']}.xpi
   fi
 
-  if [ "$ff_addon_download_url" = "" ]; then
+  if [ "${ff_lib['addon_download_url']}" = "" ]; then
     ff_log "Must specify URL to download the XPI file"
     return 1
   fi
 
   local jwt_token=$(ff_do_jwt)
 
-  set -x
+  ff_do_curl -o "${ff_lib['xpi_file']}" -g "${ff_lib['addon_download_url']}" -H "Authorization: JWT ${jwt_token}"
 
-  ff_do_curl -o "${ff_xpi_file}" -g "$ff_addon_download_url" -H "Authorization: JWT ${jwt_token}"
-
-  set +x
-
-  ff_log "Downloaded file saved at $ff_xpi_file"
+  ff_log "Downloaded file saved at ${ff_lib['xpi_file']}"
 
 }
 
@@ -367,20 +367,20 @@ ff_accept_new_version(){
 
   ff_parse_args "$@"
 
-  if [ "$ff_new_addon_version" = "" ]; then    
+  if [ "${ff_lib['new_addon_version']}" = "" ]; then    
     ff_log "New addon version not specified, terminating"
     return 1
   fi
 
-  local manifest="$ff_addon_dir/manifest.json"
+  local manifest="${ff_lib['addon_dir']}/manifest.json"
 
   if [ ! -f "$manifest" ]; then
     ff_log "Manifest not found at $manifest, terminating"
     return 1
   fi
 
-  local new_manifest=$($jq --arg new_version "$ff_new_addon_version" '.version = $new_version' < "$manifest")
+  local new_manifest=$(${ff_lib['jq']} --arg new_version "${ff_lib['new_addon_version']}" '.version = $new_version' < "$manifest")
   echo "$new_manifest" > "$manifest"
 
-  ff_log "Version $ff_new_addon_version written to manifest"
+  ff_log "Version ${ff_lib['new_addon_version']} written to manifest"
 }
